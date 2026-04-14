2bf1733299a5b9af77096243c6b68a9fea6c8a4a
Pavel Nesterov <pnestero@gmail.com>
2026-04-14 21:49:08 +0500

    До создания шаблонов

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+
+def info(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "info.html", context)
